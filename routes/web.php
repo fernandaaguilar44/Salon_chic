272ad25e46749f59f4ef3921c679befc4048eb1f
Jade Bucardo <jade.bucardo@unah.hn>
2025-06-11 09:30:32 -0600

    las rutas para solucionar un problema

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmpleadoController;
 use App\Http\Controllers\LlamadoAtencionController;
+use App\Http\Controllers\ProveedorController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -24,3 +25,11 @@ Route::controller(EmpleadoController::class)->group(function () {
 Route::get('llamados/create', [LlamadoAtencionController::class, 'create'])->name('llamados.create');
 Route::post('llamados', [LlamadoAtencionController::class, 'store'])->name('llamados.store');
 Route::get('/empleados/{id}/historial', [EmpleadoController::class, 'historial'])->name('empleados.historial');
+
+
+Route::get('/proveedores', [ProveedorController::class, 'index'])->name('proveedores.index');
+Route::get('/proveedores/create', [ProveedorController::class, 'create'])->name('proveedores.create');
+Route::post('/proveedores', [ProveedorController::class, 'store'])->name('proveedores.store');
+Route::get('/proveedores/{proveedor}', [ProveedorController::class, 'show'])->name('proveedores.show');
+Route::get('/proveedores/{proveedor}/edit', [ProveedorController::class, 'edit'])->name('proveedores.edit');
+Route::put('/proveedores/{proveedor}', [ProveedorController::class, 'update'])->name('proveedores.update');
